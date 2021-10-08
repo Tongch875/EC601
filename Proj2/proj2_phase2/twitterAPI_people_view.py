c52d76fe1d56e8744652c7d97f4a6e199264eadd
Tongch875 <80622218+Tongch875@users.noreply.github.com>
2021-10-08 14:07:15 -0400

    Update twitterAPI_people_view.py

--- Proj2/proj2_phase2/twitterAPI_people_view.py
+++ Proj2/proj2_phase2/twitterAPI_people_view.py
@@ -4,7 +4,7 @@ def people_view():
     import json
     import sys
 
-    # input the keys and secret to acquire permission of API
+    # input the keys and secret  API
     consumerKey = ""
     consumerSecret = ""
     accessToken = ""
@@ -16,18 +16,18 @@ def people_view():
     KEY = input(" Input KEYWORDS that you want to find: ")
     NUM = int(input("Input the NUMBER that you need : "))
 
-    LIST = []  # creat a new list to save data
+    LIST_DATA = []  # creat a new list to save data
 
-    search = tweepy.Cursor(api.search_tweets, KEY).items(NUM)  # use the API of search
+    search = tweepy.Cursor(api.search_tweets, KEY).items(NUM)  #  API  search
 
     for tweets in search:
-        LIST.append([username(tweets.text) ,tweets.text]) # alltweets.text #tweets.id
+        LIST_DATA.append([username(tweets.text) ,tweets.text]) # alltweets.text #tweets.id
 
     # print("This is what you want to find :")
     # for i in LIST:
     #     print(i)
 
-    return LIST, KEY
+    return LIST_DATA, KEY
 
 def username(text):
     start = text.find("@")
