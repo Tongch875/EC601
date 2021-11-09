45b78c3d6cc2fa928526c3bf1750bc00356de0ca
Tongch875 <80622218+Tongch875@users.noreply.github.com>
2021-11-08 22:21:57 -0500

    Update test_ea.py

--- Proj2/proj2_phase2/test_ea.py
+++ Proj2/proj2_phase2/test_ea.py
@@ -1,3 +1,4 @@
+#Manually entering the keywords and number of Twitter searches in the source program is needed. Please change the input part of the program to a constant when testing!
 import emotion_analyze as ea
 import pytest
 
